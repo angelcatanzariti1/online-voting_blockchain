d00ab111fb7797ff348ed512353e17712d84e1e8
Angel Catanzariti <angel@innship.com>
2022-01-19 18:10:27 -0300

    view votes per candidate

--- voting.sol
+++ voting.sol
@@ -57,4 +57,35 @@ contract voting{
     function ViewVotes(string memory _candidate) public view returns(uint){
         return(candidateVotes[_candidate]);
     }
+    
+    // VIEW RESULTS -----------------------------------------
+    function ViewResults() public view returns(string memory){
+        string memory results = "";
+        
+        for(uint i=0; i < candidates.length; i++){
+            results = string(abi.encodePacked(results, "(", candidates[i], ", ", uint2str(ViewVotes(candidates[i]))));
+        }
+
+        return(results);
+    }
+
+    // AUX --------------------------------------------------
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
 }
